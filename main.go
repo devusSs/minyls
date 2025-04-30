38939279c46285d0231f4704df3832641f3ee11b
devusSs <flofest98@gmail.com>
2025-04-30 23:20:21 +0200

    added commands layout, help & version printing

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 var (
 	buildVersion   string
@@ -23,5 +27,76 @@ func init() {
 }
 
 func main() {
-	fmt.Printf("init: version: %s, date: %s, commit: %s\n", buildVersion, buildDate, buildGitCommit)
+	if len(os.Args) < 2 {
+		fmt.Println("error: no command provided")
+		fmt.Println()
+		printHelp()
+		os.Exit(1)
+	}
+
+	handleCommandLine()
+}
+
+const (
+	appName        = "minyls"
+	appDescription = "Go tool to combine MinIO and YOURLS."
+	appGithubLink  = "github.com/devusSs/minyls"
+)
+
+func printHelp() {
+	fmt.Printf("%s - %s\n", appName, appDescription)
+	fmt.Println()
+	fmt.Println(appGithubLink)
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println("	minyls <command> <parameters>")
+	fmt.Println()
+	fmt.Println("Available commands and parameters:")
+	fmt.Println("	help")
+	fmt.Println("	version")
+	fmt.Println("	upload		[filepath] [policy]")
+	fmt.Println("	list")
+	fmt.Println("	download	[id] [filepath]")
+	fmt.Println("	delete		[id]")
+	fmt.Println("	clear		[option]")
+}
+
+func handleCommandLine() {
+	command := os.Args[1]
+
+	switch command {
+	case "help":
+		printHelp()
+	case "version":
+		printVersion()
+	case "upload":
+		fmt.Println("upload command, not implemented")
+	case "list":
+		fmt.Println("list command, not implemented")
+	case "download":
+		fmt.Println("download command, not implemented")
+	case "delete":
+		fmt.Println("delete command, not implemented")
+	case "clear":
+		fmt.Println("clear command, not implemented")
+	default:
+		fmt.Println("error: unrecognized command:", command)
+		fmt.Println()
+		printHelp()
+		os.Exit(1)
+	}
+}
+
+func printVersion() {
+	fmt.Printf("%s - %s\n", appName, appDescription)
+	fmt.Println()
+	fmt.Println(appGithubLink)
+	fmt.Println()
+	fmt.Printf("Build version:\t\t%s\n", buildVersion)
+	fmt.Printf("Build date:\t\t%s\n", buildDate)
+	fmt.Printf("Build Git commit:\t%s\n", buildGitCommit)
+	fmt.Println()
+	fmt.Printf("Build Go version:\t%s\n", runtime.Version())
+	fmt.Printf("Build Go OS:\t\t%s\n", runtime.GOOS)
+	fmt.Printf("Build Go arch:\t\t%s\n", runtime.GOARCH)
 }
