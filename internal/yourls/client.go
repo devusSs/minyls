f8120ebf2df18ce02f7f9e07ec500ea28fe3650a
devusSs <flofest98@gmail.com>
2025-05-01 23:18:22 +0200

    added yourls client, helper and shorten functions

---
+++ internal/yourls/client.go
@@ -0,0 +1,67 @@
+package yourls
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// TODO: make http client configurable
+// via options on NewClient
+type Client struct {
+	endpoint  string
+	signature string
+	client    *http.Client
+}
+
+func NewClient(endpoint string, signature string) *Client {
+	return &Client{endpoint, signature, http.DefaultClient}
+}
+
+// since all api calls are post we can simply build
+// a request using a context and url.Values
+func (c *Client) doAPIRequest(
+	ctx context.Context,
+	values map[string]interface{},
+) (*http.Response, error) {
+	v, err := convertValues(values)
+	if err != nil {
+		return nil, fmt.Errorf("could not convert values: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.endpoint, valuesToReader(v))
+	if err != nil {
+		return nil, fmt.Errorf("could not create request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("could not get response: %w", err)
+	}
+
+	return resp, nil
+}
+
+func convertValues(values map[string]interface{}) (url.Values, error) {
+	v := url.Values{}
+	for key, value := range values {
+		c, ok := value.(string)
+		if !ok {
+			return nil, fmt.Errorf("could not type cast '%v' to string", value)
+		}
+
+		v.Set(key, c)
+	}
+
+	return v, nil
+}
+
+func valuesToReader(values url.Values) io.Reader {
+	return strings.NewReader(values.Encode())
+}
---
+++ internal/yourls/client_shorten.go
@@ -0,0 +1,72 @@
+package yourls
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/google/uuid"
+)
+
+// Shorten takes in a url and returns a shortened url with the title set internally on YOURLS.
+//
+// Since YOURLS does not support expiry by default, the shortened URL
+// should be deleted manually / by this program after the MinIO presigned url expires.
+func (c *Client) Shorten(ctx context.Context, input string, title string) (string, error) {
+	u, err := url.Parse(input)
+	if err != nil {
+		return "", fmt.Errorf("invalid input provided: %w", err)
+	}
+
+	uid, err := uuid.NewUUID()
+	if err != nil {
+		return "", fmt.Errorf("could not generate a uuid for keyword: %w", err)
+	}
+
+	v := make(map[string]interface{})
+	v["signature"] = c.signature
+	v["action"] = "shorturl"
+	v["format"] = "json"
+	v["url"] = u
+	v["title"] = title
+	v["keyword"] = uid
+
+	resp, err := c.doAPIRequest(ctx, v)
+	if err != nil {
+		return "", fmt.Errorf("failed to do api request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf(
+			"unexpected status code: %d (status: %s)",
+			resp.StatusCode,
+			resp.Status,
+		)
+	}
+
+	res := &shortenURLResponse{}
+	err = json.NewDecoder(resp.Body).Decode(res)
+	if err != nil {
+		return "", fmt.Errorf("could not decode json response: %w", err)
+	}
+
+	return res.Shorturl, nil
+}
+
+type shortenURLResponse struct {
+	URL struct {
+		Keyword string `json:"keyword"`
+		URL     string `json:"url"`
+		Title   string `json:"title"`
+		Date    string `json:"date"`
+		IP      string `json:"ip"`
+	} `json:"url"`
+	Status     string `json:"status"`
+	Message    string `json:"message"`
+	Title      string `json:"title"`
+	Shorturl   string `json:"shorturl"`
+	StatusCode string `json:"statusCode"`
+}
